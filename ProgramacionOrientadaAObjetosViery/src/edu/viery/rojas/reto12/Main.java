dcc8dfe3b083bf8f36c264f11521c84ff290b7e3
VieryDanielRojasHernandezx <123215273+VieryDanielRojasHernandezx@users.noreply.github.com>
2023-05-10 23:56:31 -0600

    Reto 12

---
+++ ProgramacionOrientadaAObjetosViery/src/edu/viery/rojas/reto12/Main.java
@@ -0,0 +1,16 @@
+package edu.viery.rojas.reto12;
+
+import edu.viery.rojas.reto12.ui.CLI;
+
+public class Main {
+    public static void main(String[] args) {
+        {
+            CLI.main();
+        }
+    }
+}
+
+
+
+
+
